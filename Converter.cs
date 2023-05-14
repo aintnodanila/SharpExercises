6aff4a3b2553d54cff793048af52f6ebae0e3b7c
Danila Rusak <skinny4550@gmail.com>
2023-05-14 14:12:21 +0200

    Update Conventer.cs

--- Converter.cs
+++ Converter.cs
@@ -1,23 +1,23 @@
 namespace SharpExercises;
 public class Converter
 {
-    private string[] Romans = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
-    private int[] Arabics = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+    private readonly string[] _romanNumbers = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+    private readonly int[] _arabicNumbers = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
 
     public int ConvertToArabic(string romanNumeral)
     {
         int arabicNumber = 0;
-        int index = 0;
-
-        foreach (string roman in Romans)
+        int i = 0;
+        
+        foreach (string roman in _romanNumbers)
         {
             while (romanNumeral.StartsWith(roman))
             {
-                arabicNumber += Arabics[index];
+                arabicNumber += _arabicNumbers[i];
                 romanNumeral = romanNumeral.Substring(roman.Length);
             }
 
-            index++;
+            i++;
         }
 
         return arabicNumber;
--- Program.cs
+++ Program.cs
@@ -4,22 +4,26 @@ public class Program
 { 
     public static void Main() 
     {
-        // Internet trolls 
-        //InternetTrolls i = new InternetTrolls();
-        //Console.WriteLine(i.RemoveVowels("This website is for loosers!"));
-        //Console.WriteLine(i.RemoveVowels("You're so dumb."));
-
+        // Internet trolls  
+        /*
+        InternetTrolls i = new InternetTrolls();
+        Console.WriteLine(i.RemoveVowels("This website is for loosers!"));
+        Console.WriteLine(i.RemoveVowels("You're so dumb."));
+        */
         
         // Roman numbers converter  
-        //Converter c = new Converter();
-        //Console.WriteLine(c.ConvertToArabic("MDCLXVI"));
-        //Console.WriteLine(c.ConvertToArabic("MCMXC"));
-
+        /*
+        Converter c = new Converter();
+        Console.WriteLine(c.ConvertToArabic("MDCLXVI"));
+        Console.WriteLine(c.ConvertToArabic("MCMXC"));
+        */
         
         // Phone numbers checking
-        //PhoneNumbers p = new PhoneNumbers();
-        //Console.WriteLine(p.CheckNumber("efRFS:)0207ERGQREG88349F82"));
-        //Console.WriteLine(p.CheckNumber("1111111EDEDEDEBGBGB232323TRTR"));
+        /*
+        PhoneNumbers p = new PhoneNumbers();
+        Console.WriteLine(p.CheckNumber("efRFS:)0207ERGQREG88349F82"));
+        Console.WriteLine(p.CheckNumber("1111111EDEDEDEBGBGB232323TRTR"));
+        */
     }
 }
 
